14ecc12049e3147df5c4aa4a4967d0343babcfe5
GK1023PRO <202311962@ua.edu.lb>
2026-06-02 01:22:44 +0300

    add node app

---
+++ node-app/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Kubernetes Node.js App 🚀",
+    status: "running",
+    version: "1.0"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
